70f0d4611e1ddf0e0c5504aa7388ae3764574f0a
Rafael Vastag <rgvastag@gmail.com>
2022-01-05 12:21:59 -0300

    push'

--- RedisCache.Function/CacheReadFunction.cs
+++ RedisCache.Function/CacheReadFunction.cs
@@ -9,7 +9,6 @@ using Newtonsoft.Json;
 using RedisCache.Function.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace RedisCache.Function
